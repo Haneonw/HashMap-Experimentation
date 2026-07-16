9e0c13ee2de0b4d0d5a9d6fc56e346d935cf6765
DeborahArauj0 <deborah.araujo@ccc.ufcg.edu.br>
2026-07-16 16:40:55 -0300

    teste incial: compara colisoes entre Modular e Multiplicativa com chaves pequenas

--- Benchmark.java
+++ Benchmark.java
@@ -1,6 +1,7 @@
 public class Benchmark{
     public static void main(String[] args){
         FuncaoHash modular = new Modular(10);
+        FuncaoHash multiplicativa = new Multiplicativa(10);
 
         modular.put(0, null);
         modular.put(1, null);
@@ -9,6 +10,14 @@ public class Benchmark{
         modular.put(25, null);
         modular.put(11, null);
 
+        multiplicativa.put(0, null);
+        multiplicativa.put(1, null);
+        multiplicativa.put(5, null);
+        multiplicativa.put(15, null);
+        multiplicativa.put(25, null);
+        multiplicativa.put(11, null);
+
         System.out.print(modular.toString());
+        System.out.println(multiplicativa.toString());
     }
 }
