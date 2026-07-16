e3a281e2172464de5a03521149646aa25722dd7b
DeborahArauj0 <deborah.araujo@ccc.ufcg.edu.br>
2026-07-16 16:17:40 -0300

    inicia implementação função hash multiplicativa

--- Multiplicativa.java
+++ Multiplicativa.java
@@ -1,4 +1,8 @@
 public class Multiplicativa extends FuncaoHash{
+
+    //Constante 
+    private static final double A = (Math.sqrt(5) - 1 / 2);
+
     public Multiplicativa(int size){
         super(size);
         this.nomeFunc = "Multiplicativa";
@@ -6,6 +10,15 @@ public class Multiplicativa extends FuncaoHash{
 
     @Override
     protected int hash(int chave) {
-        return 0;
+        //passo 1: k * A
+        double produto = chave * A;
+
+        //passo 2 parte fracionária 
+        double fracao = produto - Math.floor(produto);
+
+        //passo 3 e 4: m * fracao(kA), depois a parte inteira
+        int indice = (int) Math.floor(this.size * fracao);
+        
+        return indice;
     }
 }
