c54871e76bc5189cdb1846e83ac04e775018409a
Haneonw <145877776+Haneonw@users.noreply.github.com>
2026-07-22 14:54:26 -0300

    Implementa uma demo do leitor de arquivos para testes

--- Benchmark.java
+++ Benchmark.java
@@ -1,16 +1,38 @@
 import hash.*;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Benchmark{
-    public static void main(String[] args){
-        int tamanho = 10;
-        
-        FuncaoHash modular = new Modular(4);
-        FuncaoHash multiplicativa = new Multiplicativa(4);
-        FuncaoHash meioDoQuadrado = new MeioDoQuadrado(4);
-        FuncaoHash Universal = new Universal(4);
-        int[] chaves = {0, 1, 5, 15, 25,11 ,13 ,41 ,23 ,21 ,1 ,2 ,432};
-        FuncaoHash[] funcoes = {modular, multiplicativa, meioDoQuadrado, Universal};
+    public static void main(String[] args)throws IOException{
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
 
+        // Usuário escolhe qual vai ser o tamanho fixo da tabela hash.
+        System.out.print("Tamanho da tabela: ");
+
+        ArrayList<Integer> chaves = new ArrayList<>();
+
+        int tamanho = Integer.parseInt(entrada.readLine());   
+        
+        // Coleta de chaves de um arquivo.
+        System.out.print("Escolha um arquivo com as chaves: ");
+
+        String arquivo = entrada.readLine();
+        
+        chaves = carregarChaves(arquivo);
+
+        // Todas as funções hash que serão testadas:
+        FuncaoHash modular = new Modular(tamanho);
+        FuncaoHash multiplicativa = new Multiplicativa(tamanho);
+        FuncaoHash meioDoQuadrado = new MeioDoQuadrado(tamanho);
+        FuncaoHash murmurHash3 = new MurmurHash3(tamanho);
+        FuncaoHash folding = new Folding(tamanho);
+        FuncaoHash universal = new Universal(tamanho);
+        FuncaoHash[] funcoes = {modular, multiplicativa, meioDoQuadrado, murmurHash3, universal, folding};
+
+        // Inserindo as chaves em cada tabela hash.
         for(FuncaoHash funcao: funcoes){
             for(int chave: chaves){
                 funcao.put(chave, null);
@@ -20,5 +42,19 @@ public class Benchmark{
             System.out.println(funcao);
         }
         System.out.println();
+   }
+
+    private static ArrayList<Integer> carregarChaves(String arquivo) throws IOException
+   {
+        BufferedReader arq = new BufferedReader(new FileReader("dados/" + arquivo));
+
+        ArrayList<Integer> chaves = new ArrayList<>();
+
+        String linha;
+        while((linha = arq.readLine()) != null && !linha.isEmpty()){
+             chaves.add(Integer.parseInt(linha));
+        }
+
+        return chaves;
    }
 }
